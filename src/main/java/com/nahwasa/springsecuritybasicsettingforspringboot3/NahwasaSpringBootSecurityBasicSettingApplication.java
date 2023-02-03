3e603026410210170eb98cd0af037e151df21dea
nahwasa <nahwasa@gmail.com>
2023-02-03 19:00:48 +0900

    spring initializr 기본 세팅. security, web만 디펜던시 추가.

---
+++ src/main/java/com/nahwasa/springsecuritybasicsettingforspringboot3/NahwasaSpringBootSecurityBasicSettingApplication.java
@@ -0,0 +1,13 @@
+package com.nahwasa.springsecuritybasicsettingforspringboot3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NahwasaSpringBootSecurityBasicSettingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NahwasaSpringBootSecurityBasicSettingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nahwasa/springsecuritybasicsettingforspringboot3/NahwasaSpringBootSecurityBasicSettingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nahwasa.springsecuritybasicsettingforspringboot3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NahwasaSpringBootSecurityBasicSettingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
