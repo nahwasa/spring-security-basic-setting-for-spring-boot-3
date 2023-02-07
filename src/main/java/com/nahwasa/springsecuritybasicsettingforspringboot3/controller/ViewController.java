9f8257c40e78ee725e9158f4911fbce20c0bafe0
nahwasa <nahwasa@gmail.com>
2023-02-07 23:19:22 +0900

    블로그에서 스프링 시큐리티를 붙여나가는 과정을 보기 위해 시큐리티 관련된 내용을 모두 제외한 프로젝트.

--- src/main/java/com/nahwasa/springsecuritybasicsettingforspringboot3/controller/ViewController.java
+++ src/main/java/com/nahwasa/springsecuritybasicsettingforspringboot3/controller/ViewController.java
@@ -1,9 +1,5 @@
 package com.nahwasa.springsecuritybasicsettingforspringboot3.controller;
 
-import com.nahwasa.springsecuritybasicsettingforspringboot3.config.AdminAuthorize;
-import com.nahwasa.springsecuritybasicsettingforspringboot3.config.UserAuthorize;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -24,20 +20,16 @@ public class ViewController {
     }
 
     @GetMapping("/dashboard")
-    public String dashboardPage(@AuthenticationPrincipal User user, Model model) {
-        model.addAttribute("loginId", user.getUsername());
-        model.addAttribute("loginRoles", user.getAuthorities());
+    public String dashboardPage(Model model) {
         return "dashboard";
     }
 
     @GetMapping("/setting/admin")
-    @AdminAuthorize
     public String adminSettingPage() {
         return "admin_setting";
     }
 
     @GetMapping("/setting/user")
-    @UserAuthorize
     public String userSettingPage() {
         return "user_setting";
     }
